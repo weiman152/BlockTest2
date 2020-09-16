c8e34e3fa9c434a90f68049e82739dbe83f59ab0
weiman <weiman188@163.com>
2020-09-16 15:24:57 +0800

    block作为全局变量，部分案例

--- BlockTest2/BlockTest2/Classes/VCs/home/QuanjuController.m
+++ BlockTest2/BlockTest2/Classes/VCs/home/QuanjuController.m
@@ -8,7 +8,19 @@
 
 #import "QuanjuController.h"
 
+//由于block的类型过长，一般作为全局属性的时候，会给它起一个别名
+typedef NSString * (^PrintBlock)(NSString *);
+typedef NSInteger (^SumBlock)(NSInteger);
+typedef NSInteger (^JieCBlock)(NSInteger);
+
 @interface QuanjuController ()
+//有返回值，有参数的block
+@property(nonatomic, copy)PrintBlock blockp;
+@property(nonatomic, copy)SumBlock blocks;
+@property(nonatomic, copy)JieCBlock blockj;
+@property (weak, nonatomic) IBOutlet UITextField *inputTextField;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+
 
 @end
 
@@ -16,7 +28,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    [self setup];
 }
 
 +(id)instance{
@@ -24,5 +36,45 @@
     return vc;
 }
 
+-(void)setup{
+    
+}
+
+- (IBAction)test1:(id)sender {
+    self.blockp = ^NSString *(NSString * str) {
+        NSString * temp = [NSString stringWithFormat:@"值：%@, 地址：%p", str, str];
+        return temp;
+    };
+    //调用,记得要判断，不然的话，如果block没实现，去调用它会造成崩溃
+    if(self.blockp){
+        NSString * str = self.blockp(@"你好呀");
+        NSLog(@"%@", str);
+    }
+}
+
+- (IBAction)test2:(id)sender {
+    self.blocks = ^NSInteger(NSInteger n) {
+        NSLog(@"1到 %ld 的和是：",n);
+        NSInteger result = 0;
+        while (n>0) {
+            result += n;
+            n--;
+        }
+        return result;
+    };
+    //调用
+    if (self.blocks) {
+        if(self.inputTextField.text.length>0){
+            NSInteger n = self.inputTextField.text.integerValue;
+            NSInteger sum = self.blocks(n);
+            NSLog(@"%ld",sum);
+            self.resultLabel.text = [NSString stringWithFormat:@"%ld", sum];
+        }
+    }
+}
+
+- (IBAction)test3:(id)sender {
+    
+}
 
 @end
